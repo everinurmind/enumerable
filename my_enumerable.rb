acca97be8eef9a82e18e7a6fbaa75d0a4dbaeba7
everinurmind <nurbol.sultanov@gmail.com>
2023-05-26 10:11:23 -0600

    Fix rubocop linters (2)

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This module provides enumerable methods for collections.
 module MyEnumerable
   def all?
--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'my_enumerable'
 
 # Represents a list that includes the MyEnumerable module.
@@ -10,7 +8,7 @@ class MyList
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
+  def each(&)
+    @list.each(&)
   end
 end
